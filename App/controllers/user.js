b2870405c975cc9c5007aef1104dea6c080255a6
Albajunior <guissealba@gmail.com>
2023-04-25 15:32:19 +0200

    bcrypt

--- App/controllers/user.js
+++ App/controllers/user.js
@@ -1,8 +1,9 @@
-const User = require("../models/user");
+const { User } = require("../models");
 const bcrypt = require('bcrypt');
 
 exports.signup = async (req, res) => {
     try {
+        console.log(req.body);
         req.body.password = await bcrypt.hash(req.body.password, 10);
         const user = await User.create(req.body);
         res.send(user);
--- App/seeders/20230424125258-demo-user.js
+++ App/seeders/20230424125258-demo-user.js
@@ -1,24 +1,22 @@
 const bcrypt = require('bcrypt');
-const hashedPassword =  bcrypt.hash(password, 10);
 
 'use strict';
 
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  
+  async up(queryInterface, Sequelize) {
+    const hashedPass = await bcrypt.hash("123456", 10);
     await queryInterface.bulkInsert('Users', [{
-      firstname: 'Johnnn',
-      lastname: 'Doett',
+      firstname: 'John',
+      lastname: 'Doe',
       email: "john.doe@mail.com",
-      password: hashedPassword("123456"),
-      createdAt: new Date(),
-      updatedAt: new Date()
-
+      password: hashedPass 
     }], {});
   },
-  
-  async down (queryInterface, Sequelize) {
-      await queryInterface.bulkDelete('Users', null, {});
-    }
-  };
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
