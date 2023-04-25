ba68b7e4859d1332fa86bb8217d3618421bc98a4
Albajunior <guissealba@gmail.com>
2023-04-25 13:07:30 +0200

    controller

--- App/routes/wood.js
+++ App/routes/wood.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express();
+const woodCtrl = require('../controllers/wood');
 
 
-router.get('/list', function (req, res) {
-  res.send('List');
-});
+router.get('/list', woodCtrl.list);
 
 module.exports = router;
