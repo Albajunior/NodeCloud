df5f302143e32943b49e3c110eb8d0c968fabd7a
Albajunior <guissealba@gmail.com>
2023-04-26 15:20:02 +0200

    get/post

--- App/controllers/wood.js
+++ App/controllers/wood.js
@@ -1,5 +1,4 @@
 const { Wood } = require("../models");
-//const pathname = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
 
 exports.readAll = async (req, res) => {
   try {
@@ -8,37 +7,35 @@ exports.readAll = async (req, res) => {
     res.status(200).json(wood);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ erreur: 'Erreur lors de la récupération' });
+    res.status(500).json({ erreur: "Erreur lors de la récupération" });
   }
-
 };
 
 exports.findByHardness = async (req, res) => {
   try {
     console.log(req.body);
-    
+
     const wood = await Wood.findAll({
       where: {
-        hardness: req.params.hardness
-      }
+        hardness: req.params.hardness,
+      },
     });
     res.status(200).json(wood);
   } catch (error) {
     console.error(err);
-    res.status(500).json({ erreur: 'Erreur lors de la récupération' });
+    res.status(500).json({ erreur: "Erreur lors de la récupération" });
   }
-
-};  
+};
 
 exports.createWood = async (req, res) => {
+  const pathname = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
   try {
     console.log(req.body);
-    //req.body.password = await bcrypt.hash(req.body.password, 10);
     const wood = await Wood.create(req.body);
-    res.status(201).json(w);
-    //res.status(201).json.parse(req.body.datas)
+    
+    res.status(200).json.parse(req.body.datas)
   } catch (error) {
     console.error(err);
-    res.status(500).json({ erreur: "Erreur lors de la récupération" });
+    res.status(500).json({ erreur: "Erreur " });
   }
 };
